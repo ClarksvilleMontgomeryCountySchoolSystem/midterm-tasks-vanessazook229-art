5f6e0f243363f4b4defafa32fce748ff014b457a
vanessazook229-art <vanessa.zook229@students.cmcss.net>
2025-12-18 13:49:43 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+slices = party_pizza_mini + large + medium
+print(f'Total number of slices: {slices}')
 
+people = people + 1
+share = slices // people
+leftover = share % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+
+people = people + 2 #Eric and Brandon are coming too.
+share = round(slices // people)
+leftover = share % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+
+
+
+slices = slices + party_pizza_mini
+share = slices // people
+leftover = share % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+print("...for Mr. Hollow Leg")
